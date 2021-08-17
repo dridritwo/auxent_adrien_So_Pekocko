6de2e7f4182cb18a79b0e9bbb92837487eb2739b
ADEO\20014103 <adrien.auxent@adeo.com>
2021-08-17 13:24:53 +0200

    secret token

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -31,7 +31,7 @@ exports.logIn = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            token: jwt.sign({ userId: user._id }, process.env.JWT_TOKEN_SECRET, {
               expiresIn: "24h",
             }),
           });
